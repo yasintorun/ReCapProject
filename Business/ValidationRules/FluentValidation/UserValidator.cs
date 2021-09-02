b9a22809799fcddeb4afa594fbe96c1b7ed97f58
yasintorun <yasintorun4680@gmail.com>
2021-09-02 10:58:24 +0300

    FluentValidation eklendi.
    12. Gün ödev 2 tamamlandı.

---
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.Email).NotNull();
+            RuleFor(u => u.Password).NotNull().MinimumLength(6);
+            RuleFor(u => u.Firstname).NotNull().MinimumLength(3);
+            RuleFor(u => u.Lastname).NotNull().MinimumLength(2);
+        }
+    }
+}
